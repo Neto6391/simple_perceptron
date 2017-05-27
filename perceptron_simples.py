4847c07004e27ebea30475216589f0831a930822
Neto6391 <neto6391@gmail.com>
2017-05-27 13:39:02 -0300

    arquivo definitivo

--- perceptron_simples.py
+++ perceptron_simples.py
@@ -5,12 +5,14 @@ def fatiarArquivo(file):
 	f_split = [f.replace(",",".") for f in f_split]
 	return(f_split)
 
+
 def listarTreinamento(file_split):
 	lista_treinamento = []
 	for row in file[1:len(file)-1]:
 		split_list =  [float(r) for r in row.split(";")]
 		split_list.pop(3)
 		lista_treinamento.append(split_list)
+	
 	return(lista_treinamento)
 
 def listarSaida(file_split):
@@ -21,7 +23,7 @@ def listarSaida(file_split):
 	return(lista_saida)
 
 def sinal(u):
-	return 1 if u >= 0 else -1
+	return 1.0 if u >= 0 else -1.0
 
 def lerArquivo(file_dataset):
 	f = open(file_dataset).read()
@@ -33,37 +35,49 @@ def lerArquivo(file_dataset):
 file = lerArquivo(file_dataset="dataset_treinamento.csv")
 
 conjunto_amostras = listarTreinamento(file)
-
+num_amostra = len(conjunto_amostras[0])
 conjunto_saida = listarSaida(file)
-for i in range(len(conjunto_amostras)):
-	conjunto_amostras[i].insert(0,-1)
 
-print("Carregando %d amostras de dados\nCom %d dados de saída\n" %(len(conjunto_amostras), len(conjunto_saida)))
-print(conjunto_amostras)
-	
-w = [random() for i in range(len(conjunto_amostras[0]))]
-w.insert(0, -1)
-print("Peso ", w)
+print(conjunto_amostras[0])
+
+for amostra in conjunto_amostras:
+	amostra.insert(0,-1)
+	print(amostra)
+
+peso = [random() for i in range(num_amostra)]
+peso.insert(0, -1)
+
+
+print("\nCarregando %d amostras de dados\nCom %d dados de saída\n" %(len(conjunto_amostras), len(conjunto_saida)))
+
 num_epoca = 0
 epoca = 1000
 taxa_aprendizado = 0.1
-erro = True
-while num_epoca < epoca or not erro:
+
+while True:
+
+	erro = False
 	
 	for i in range(len(conjunto_amostras)):
 		
 		u = 0
 		
-		for j in range(len(conjunto_amostras[0])):
-			u += w[j] * conjunto_amostras[i][j]
+		for j in range(num_amostra + 1):
+			u += peso[j] * conjunto_amostras[i][j]
 			
 		y = sinal(u)
+		print("Função de ativação", y)
 		if y != conjunto_saida[i]:
 			erro_aux = conjunto_saida[i] - y
 
-			for j in range(len(conjunto_amostras[0])):
+			for j in range(num_amostra + 1):
+				peso[j] = peso[j] + taxa_aprendizado * erro_aux * conjunto_amostras[i][j]
 
-				w[j] = w[j] + taxa_aprendizado * erro_aux * conjunto_amostras[i][j]
-				erro = True
-		print(num_epoca)
-		num_epoca += 1
+			erro = True
+	print(num_epoca)
+	num_epoca += 1
+
+	if num_epoca > epoca or not erro:
+		break
+
+print("\nSaida ", conjunto_saida)
