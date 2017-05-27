11698644e26fea1286737a3efab0a3e84ffa7f9a
Neto6391 <neto6391@gmail.com>
2017-05-27 11:11:38 -0300

    Primeiro commit

---
+++ perceptron_simples.py
@@ -0,0 +1,69 @@
+from random import random
+
+def fatiarArquivo(file):
+	f_split = file.split("\n")
+	f_split = [f.replace(",",".") for f in f_split]
+	return(f_split)
+
+def listarTreinamento(file_split):
+	lista_treinamento = []
+	for row in file[1:len(file)-1]:
+		split_list =  [float(r) for r in row.split(";")]
+		split_list.pop(3)
+		lista_treinamento.append(split_list)
+	return(lista_treinamento)
+
+def listarSaida(file_split):
+	lista_saida = []
+	for row in file[1:len(file)-1]:
+		split_list =  [float(r) for r in row.split(";")]
+		lista_saida.append(split_list[3])
+	return(lista_saida)
+
+def sinal(u):
+	return 1 if u >= 0 else -1
+
+def lerArquivo(file_dataset):
+	f = open(file_dataset).read()
+	file = fatiarArquivo(f)
+	return(file)
+
+
+
+file = lerArquivo(file_dataset="dataset_treinamento.csv")
+
+conjunto_amostras = listarTreinamento(file)
+
+conjunto_saida = listarSaida(file)
+for i in range(len(conjunto_amostras)):
+	conjunto_amostras[i].insert(0,-1)
+
+print("Carregando %d amostras de dados\nCom %d dados de saída\n" %(len(conjunto_amostras), len(conjunto_saida)))
+print(conjunto_amostras)
+	
+w = [random() for i in range(len(conjunto_amostras[0]))]
+w.insert(0, -1)
+print("Peso ", w)
+num_epoca = 0
+epoca = 1000
+taxa_aprendizado = 0.1
+erro = True
+while num_epoca < epoca or not erro:
+	
+	for i in range(len(conjunto_amostras)):
+		
+		u = 0
+		
+		for j in range(len(conjunto_amostras[0])):
+			u += w[j] * conjunto_amostras[i][j]
+			
+		y = sinal(u)
+		if y != conjunto_saida[i]:
+			erro_aux = conjunto_saida[i] - y
+
+			for j in range(len(conjunto_amostras[0])):
+
+				w[j] = w[j] + taxa_aprendizado * erro_aux * conjunto_amostras[i][j]
+				erro = True
+		print(num_epoca)
+		num_epoca += 1
